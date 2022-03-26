9e17157fca46d579f38e60ea86a02152320d143c
Bogdan Silvasan <bogdan.silve@gmail.com>
2022-03-27 00:24:05 +0200

    Partial file page layout

---
+++ ClientApp/src/assets/ServerIcon.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const ServerIcon = () => {
+  return (
+    <svg
+      enableBackground="new 0 0 440 440"
+      version="1.1"
+      viewBox="0 0 440 440"
+      xmlSpace="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="60" y="390" width="320" height="40" fill="#E7ECED" />
+      <rect x="60" y="250" width="320" height="40" fill="#E7ECED" />
+      <rect x="60" y="110" width="320" height="40" fill="#E7ECED" />
+      <polygon
+        points="60 290 380 290 420 290 420 390 380 390 60 390 20 390 20 290"
+        fill="#AFB6BB"
+      />
+      <polygon
+        points="20 250 20 180 20 150 60 150 380 150 420 150 420 250 380 250 60 250"
+        fill="#AFB6BB"
+      />
+      <polygon
+        points="20 10 420 10 420 110 380 110 60 110 20 110"
+        fill="#AFB6BB"
+      />
+
+      <g fill="#231F20">
+        <path d="m420 120c5.522 0 10-4.478 10-10v-100c0-5.523-4.478-10-10-10h-400c-5.523 0-10 4.477-10 10v100c0 5.522 4.477 10 10 10h30v20h-30c-5.523 0-10 4.477-10 10v100c0 5.523 4.477 10 10 10h30v20h-30c-5.523 0-10 4.478-10 10v100c0 5.522 4.477 10 10 10h30v30c0 5.522 4.478 10 10 10h320c5.522 0 10-4.478 10-10v-30h30c5.522 0 10-4.478 10-10v-100c0-5.522-4.478-10-10-10h-30v-20h30c5.522 0 10-4.477 10-10v-100c0-5.523-4.478-10-10-10h-30v-20h30zm-390-100h380v80h-380v-80zm340 400h-300v-20h300v20zm40-40h-380v-80h380v80zm-40-100h-300v-20h300v20zm40-40h-380v-50h345v-20h-345v-10h380v80zm-40-100h-300v-20h300v20z" />
+        <rect x="50" y="35" width="160" height="20" />
+        <rect x="50" y="65" width="160" height="20" />
+        <rect x="240" y="35" width="20" height="50" />
+        <rect x="280" y="35" width="20" height="50" />
+        <rect x="320" y="35" width="20" height="50" />
+        <rect x="360" y="35" width="20" height="50" />
+        <rect x="50" y="315" width="160" height="20" />
+        <rect x="50" y="345" width="160" height="20" />
+        <rect x="240" y="315" width="20" height="50" />
+        <rect x="280" y="315" width="20" height="50" />
+        <rect x="320" y="315" width="20" height="50" />
+        <rect x="360" y="315" width="20" height="50" />
+        <rect x="45" y="205" width="25" height="20" />
+        <rect x="85" y="205" width="25" height="20" />
+        <rect x="125" y="205" width="25" height="20" />
+        <rect x="165" y="205" width="25" height="20" />
+        <rect x="205" y="205" width="25" height="20" />
+        <rect x="245" y="205" width="25" height="20" />
+        <rect x="315" y="205" width="80" height="20" />
+      </g>
+    </svg>
+  );
+};
--- ClientApp/src/pages/Files.js
+++ ClientApp/src/pages/Files.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Files.css";
+import { ServerIcon } from "../assets/ServerIcon";
+import { PlusIcon } from "../assets/PlusIcon";
 
 export const Files = () => {
-  return <div>Files</div>;
+  const [serverNames, setServerNames] = useState([
+    "filehostvm1",
+    "filehostvm2",
+    "filehostvm3",
+  ]);
+
+  return (
+    <>
+      <nav className="side-navbar">
+        <div className="servers-container">
+          <h1>File Manager</h1>
+          <div>
+            {serverNames.map((server) => (
+              <div className="server-entry">
+                <div style={{ width: "30px", height: "30px" }}>
+                  <ServerIcon />
+                </div>
+                <p style={{ marginLeft: "15px" }}>{server}</p>
+              </div>
+            ))}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              gap: "10px",
+            }}
+          >
+            <div style={{ width: "30px" }}>
+              <PlusIcon />
+            </div>
+            <b style={{ marginTop: "2px" }}>Connect to a server</b>
+          </div>
+        </div>
+        <div className="greet-container"></div>
+      </nav>
+      <header className="header">Lol ce e cu mine aici</header>
+      <div className="file-content">dwdwdw</div>
+    </>
+  );
 };
