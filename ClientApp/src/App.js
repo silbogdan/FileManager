f25ac6a6ae7766f783f9394e9ac068caa5b9635c
Bogdan Silvasan <bogdan.silve@gmail.com>
2022-03-26 19:56:05 +0200

    Boilerplate for pages

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React from "react";
+import { Switch, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Files } from "./pages/Files";
 
-import './custom.css'
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/files" component={Files} />
+    </Switch>
+  );
+};
