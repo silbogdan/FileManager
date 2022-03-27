2e9381b954d964dcd354e43de8d4ad10615d1dc7
amzaandreea <amza.andreea23@yahoo.com>
2022-03-27 04:15:05 +0300

    Added hosts in claims

---
+++ Models/PermissionsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManager.Models
+{
+    public class PermissionsModel
+    {
+        public string HostName { get; set; }
+        public string Ip { get; set; }
+
+        public PermissionsModel(string hostname, string ip)
+        {
+            this.HostName = hostname;
+            this.Ip = ip;
+        }
+    }
+}
